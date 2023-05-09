e61ea09333caba51d89b3c74044d1f5f06e5c211
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-09 21:27:11 +0300

    feat: h-tag component ready

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Head from 'next/head'
+import { Htag } from "../../components";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home(): JSX.Element {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Htag tag='h1'>Текст</Htag>
     </div>
   )
 }
