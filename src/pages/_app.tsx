7e82ee1cb5879be18d2a160f79a0fa6908a1a3f7
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-09 18:32:33 +0300

    fix react component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,5 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-  <>
-      <Head>
-        <title>New demo from app</title>
-        <meta name="description" content="Demo description" />
-      </Head>
-      <Component {...pageProps} />
-  </>
-  );
-  // return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
