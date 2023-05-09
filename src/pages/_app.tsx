89332de2fbf3f8e35650138f778e90a0acb0b39d
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-09 23:25:12 +0300

    feat: button v1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,11 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return  <>
+  return  (<>
     <Head>
       <title>My real app</title>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
     </Head>
     <Component {...pageProps} />
-  </>;
+  </>);
 
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document(): JSX.Element {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
