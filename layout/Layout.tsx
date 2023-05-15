4fd6e80f5698493f8021bd620bb2edfe71245fb3
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-15 13:28:02 +0300

    grid markdown for layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -11,14 +11,14 @@ import { FunctionComponent } from 'react';
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
 
   return (
-    <>
-      <Header />      
-      <Sidebar />
-      <div>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />      
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>
         {children}
       </div>
-      <Footer />
-    </>
+      <Footer className={styles.footer} />
+    </div>
   );
 }
 
