51b70d75d1dc5e621dc5d1bfa21efa4f34dc14aa
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-21 12:49:32 +0300

    feat: menu icons

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -4,6 +4,11 @@ import styles from './Menu.module.css';
 import cn from 'classnames';
 import { MyAppContext } from '../../context/app.context';
 
+import Courses from './icons/courses.svg';
+import Services from './icons/services.svg';
+import Books from './icons/books.svg';
+import Goods from './icons/goods.svg';
+
 
 export const Menu = (): JSX.Element => {
 
@@ -11,6 +16,10 @@ export const Menu = (): JSX.Element => {
 
   return (
    <div>
+    <Courses />
+    <Services />
+    <Books />
+    <Goods />
       <ul>
         { menu.map( (m) => (<li key={m._id.secondCategory}>
           {m._id.secondCategory}
