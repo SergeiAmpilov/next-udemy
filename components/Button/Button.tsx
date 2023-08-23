8bb3eb6ee53848fcaad102ff726e927329186bbe
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-23 18:07:34 +0300

    feat: client component example

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,18 +1,28 @@
+'use client'
+
 import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
 import ArrowImg from './arrow.svg';
+import { useEffect, useState } from 'react';
 
 export const Button = ({ appearance, arrow = 'none', children, className, ...props}: ButtonProps): JSX.Element => {
+
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log('init')
+  })
   return (
     <button
+      onClick={() => setCounter(counter + 1) }
       className={ cn(className, styles.button, {
         [styles.primary]: appearance === 'primary',
         [styles.ghost]: appearance === 'ghost',
       }) }
       {...props}
     >
+      { counter }
       { children }
       { arrow !== 'none' && <span className={ cn(styles.arrow, {
         [styles.arrowright]: arrow == 'right',
