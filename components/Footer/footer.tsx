3ef9ede434020cf0440320797ded029f12c06820
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-25 13:28:39 +0300

    refactor

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -1,13 +1,17 @@
 import { FooterProps } from "./footer.props";
-import styles from './footer.module.css'
+import styles from './footer.module.css';
+import cn from 'classnames';
 
 
-export default function Footer({ children, ...props }: FooterProps): JSX.Element {
+export default function Footer({ className, children, ...props }: FooterProps): JSX.Element {
   return (
     <footer
+      className={cn(className, styles.footer)}
       {...props}
     >
-      <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum, vel?</span>
+      <span>
+        OwlTop © 2020 - {new Date().getFullYear()} Все права защищены  
+      </span>
       <a href="#" className={styles.footerlink}>Пользовательское соглашение</a>
       <a href="#" className={styles.footerlink}>Политика конфиденциальности</a>
     </footer>
