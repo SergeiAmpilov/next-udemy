7616027c444cac9daeb4c057eef14392e092c656
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-29 16:10:43 +0300

    feat: context

--- components/Sidebar/sidebar.tsx
+++ components/Sidebar/sidebar.tsx
@@ -1,14 +1,21 @@
+'use client';
+
 import { SidebarProps } from "./sidebar.props";
-// import styles from './sidebar.module.css'
+import styles from './sidebar.module.css';
+import { createContext } from "react";
+
+const SidebarContext = createContext({});
 
 
 export default function Sidebar({ children, ...props }: SidebarProps): JSX.Element {
   return (
-    <div
-      {...props}
-    >
-      Sidebar
-      {children }
-    </div>
+    <SidebarContext.Provider value={{}}>
+      <div
+        {...props}
+      >
+        Sidebar
+        {children }
+      </div>
+    </SidebarContext.Provider>
   );
 }
