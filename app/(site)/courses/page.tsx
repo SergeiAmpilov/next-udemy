2dc824dc6cbb5ae8855781436624ea873bf3e923
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-09-04 22:06:17 +0300

    courses page

--- app/(site)/courses/page.tsx
+++ app/(site)/courses/page.tsx
@@ -1,14 +1,14 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Search page'
+  title: 'Courses page'
 }
 
 
-export default function SearchPage(): JSX.Element {
+export default function CoursesPage(): JSX.Element {
   return (
     <div>
-      <h1>Search page</h1>
+      <h1>Courses page</h1>
     </div>
   );
 }
